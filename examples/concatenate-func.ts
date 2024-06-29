f7f78733d44d213420565952fcbc78add4a845aa
Muppeda <amuppeda@ttu.edu>
2024-06-29 16:26:06 -0500

    Adding Concatenating two strings function code

---
+++ examples/concatenate-func.ts
@@ -0,0 +1,7 @@
+// This code is to Function of Concatenate two string  
+
+const ConcatenateStrings = (first: string, second: string): string => {
+    return first + second;
+}
+
+console.log(ConcatenateStrings("Hello", "World"));
