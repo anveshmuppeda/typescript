f9a01121de74ee9430ec33fb8ece39d5d048596d
Muppeda <amuppeda@ttu.edu>
2024-06-29 16:02:43 -0500

    Adding different types of variables

---
+++ examples/variable-types.ts
@@ -0,0 +1,15 @@
+let id: number = 5;
+let fullname: string = "Anvesh Muppeda";
+let graduated: boolean = true;
+let ids: number[] = [1, 2, 3, 4, 5];
+let x: any = "Anvesh";
+let xArray: any[] = ["Anvesh", 5, false];
+
+console.log("Variable Type Number: " + id);
+console.log("Variable Type String: " + fullname);
+console.log("Variable Type Boolean: " + graduated);
+console.log("Variable Type Number Array: " + ids);
+console.log("Variable Type Any: " + x);
+console.log("Variable Type Any Array: " + xArray);
+console.log("Thank You!!");
+
