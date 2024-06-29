8bef7b48fb5ecfb79488514d25dc9af2ca8d279f
Muppeda <amuppeda@ttu.edu>
2024-06-29 16:36:09 -0500

    Adding first object program

---
+++ examples/03-first-object.ts
@@ -0,0 +1,11 @@
+// This program is for first Object in TypeScript  
+
+const User = {
+    id: 5,
+    name: "Anvesh",
+    age: 25,
+};
+
+console.log("User ID: ", User.id)
+console.log("User Name: ", User.name)
+console.log("User Age: ", User.age)
